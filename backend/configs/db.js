63bb511896a5792ac31e82eee5864f80fedd07f5
ApDev. <143201999+AyushPandey021@users.noreply.github.com>
2025-09-23 20:24:37 +0530

    Update

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+    const conn = await mongoose.connect("mongodb+srv://vlogayush51_db_user:Z9gDSaTAdepeGcEg@cluster0.yidlauw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
