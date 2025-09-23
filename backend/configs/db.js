effc9f9e381b60bb2ea8e5dfaac9f653576a68cd
Ayush Pandey <ayushpandey4480@gmail.com>
2025-09-23 21:34:27 +0530

    intial

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect("mongodb+srv://vlogayush51_db_user:Z9gDSaTAdepeGcEg@cluster0.yidlauw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("DB connected")
     } catch (error) {
         console.log("DB error")
--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,24 +1,22 @@
+import jwt from "jsonwebtoken";
 
-import jwt from "jsonwebtoken"
-const isAuth=async (req,res,next)=>{
-    try {
- 
-      let {token} = req.cookies
-     
-      if(!token){
-        return res.status(400).json({message:"user doesn't have token"})
-      }
-      let verifyToken = jwt.verify(token,process.env.JWT_SECRET)
-      
-      if(!verifyToken){
-        return res.status(400).json({message:"user doesn't have valid token"})
-      }
-  
-      req.userId = verifyToken.userId
-      next()
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message:`is auth error ${error}`})
+const isAuth = async (req, res, next) => {
+  try {
+    // token either from cookies or headers
+    let token = req.cookies?.token || req.headers["authorization"]?.split(" ")[1];
+
+    if (!token) {
+      return res.status(400).json({ message: "User doesn't have token" });
     }
-}
-export default isAuth
+
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = verifyToken.userId;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: `isAuth error: ${error.message}` });
+  }
+};
+
+export default isAuth;
