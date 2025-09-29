43c42c71287920a518503a0919e84fe1ca7d947a
Ayush Pandey <ayushpandey4480@gmail.com>
2025-09-29 11:25:45 +0530

    Firebase Fix

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,19 +1,16 @@
-
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCsD3V0SF8tFz-K9Esr0Cds_13VqG4g6lA",
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
   authDomain: "web-app-313ec.firebaseapp.com",
   projectId: "web-app-313ec",
   storageBucket: "web-app-313ec.firebasestorage.app",
   messagingSenderId: "358250341653",
-  appId: "1:358250341653:web:638aee493a37ef9f80b51c",
-  measurementId: "G-KFR7KB3QZR"
+  appId: "1:665916718747:web:1:358250341653:web:638aee493a37ef9f80b51c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 export {auth,provider}
